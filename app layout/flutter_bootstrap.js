974fea9e03e85eebdfeb2c809bfa9472cb14d4a2
kvitoria01 <bonetekeila@gmail.com>
2025-01-18 14:06:45 -0300

    app

--- app layout/flutter_bootstrap.js
+++ app layout/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"062b3a72fcbe76296ce19b02f58c03ba0cbb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3890088453"
+    serviceWorkerVersion: "346036648"
   }
 });
